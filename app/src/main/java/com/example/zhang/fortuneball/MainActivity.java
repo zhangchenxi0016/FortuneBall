783a5dd3774577062ce395ecc26f21883fec8c69
zhangchenxi0016 <1510745865@qq.com>
2018-03-02 14:27:21 +0800

    fortuneBall is over

--- app/src/main/java/com/example/zhang/fortuneball/MainActivity.java
+++ app/src/main/java/com/example/zhang/fortuneball/MainActivity.java
@@ -8,23 +8,58 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    String fortuneList[]={"Don't count on it","Ask again later","You may rely on it","Without a doubt","Outlook not so good","It's decidedly so","Signs point to yes","yes definitely","yes","My sources say no"};
+    TextView mFortuneText;
+    Button mGenerateFortuneButton;
+    ImageView mFortuneBallImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        // 1:
         super.onCreate(savedInstanceState);
+// 2:
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+// 3:
+        mFortuneText = (TextView) findViewById(R.id.fortuneText);
+        mFortuneBallImage = (ImageView) findViewById(R.id.fortunateImage);
+        mGenerateFortuneButton = (Button) findViewById(R.id.fortuneButton);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+// 4:
+        mGenerateFortuneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+                // 5:
+                int index = new Random().nextInt(fortuneList.length);
+                mFortuneText.setText(fortuneList[index]);
+                // 6:
+                YoYo.with(Techniques.Swing)
+                        .duration(500)
+                        .playOn(mFortuneBallImage);
+        }
         });
     }
 
